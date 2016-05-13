4fa7fe89d7176c8fc5ac5e1fd40f3105253f87ae
qq232222440 <232222440@qq.com>
2016-05-13 17:33:40 +0800

    Update and rename gulpfile to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -8,20 +8,33 @@ var htmlmin = require('gulp-htmlmin'), //html压缩
     pngcrush = require('imagemin-pngcrush'),
     minifycss = require('gulp-minify-css'),//css压缩
     md5 = require('gulp-md5-plus'),
+    os = require('os'),
     sass = require('gulp-ruby-sass'),
+    gulpOpen = require('gulp-open'),
     babel = require('gulp-babel'),//es6转换
     clean = require('gulp-clean'),//文件清除
     jshint = require('gulp-jshint'),//js检测
     uglify = require('gulp-uglify'),//js压缩
     concat = require('gulp-concat'),//文件合并
     rename = require('gulp-rename'),//文件更名
+    connect = require('gulp-connect'),
     notify = require('gulp-notify');//提示信息
- 
+var host = {
+    path: 'dest/',
+    port: 3000,
+    html: 'index.html'
+};
+//mac chrome: "Google chrome", 
+var browser = os.platform() === 'linux' ? 'Google chrome' : (
+  os.platform() === 'darwin' ? 'Google chrome' : (
+  os.platform() === 'win32' ? 'chrome' : 'firefox'));
+var pkg = require('./package.json'); 
 // 压缩html
 gulp.task('html', function() {
   return gulp.src('src/*.html')
     .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest('./dest'))
+      .pipe(connect.reload())
     .pipe(notify({ message: 'html task ok' }));
  
 });
@@ -42,12 +55,13 @@ gulp.task('img', function() {
 gulp.task('css', function() {
     var cssSrc='src/sass/*.scss',
         cssDst = 'dest/css';
-    return gulp.src(cssDst)
+    return gulp.src('src/css/*.css')
     // .pipe(sass({ style: 'compressed'}))
-    //.pipe(gulp.dest(cssDst))
+    .pipe(gulp.dest(cssDst))
     // .pipe(rename({ suffix: '.min' }))
     .pipe(minifycss())
     .pipe(gulp.dest(cssDst))
+        .pipe(connect.reload())
     .pipe(notify({ message: 'css task ok' }));
 });
  
@@ -64,10 +78,11 @@ gulp.task('js', function() {
   return gulp.src('src/js/*.js')
     .pipe(babel({presets:['es2015']}))
    // .pipe(concat('all.js'))
-    //.pipe(gulp.dest('dest/js'))
+    .pipe(gulp.dest('dest/js'))
     // .pipe(rename({ suffix: '.min' }))
     .pipe(uglify())
     .pipe(gulp.dest('dest/js'))
+      .pipe(connect.reload())
     .pipe(notify({ message: 'js task ok' }));
 });
 //将js加上10位md5,并修改html中的引用路径，该动作依赖js
@@ -85,13 +100,30 @@ gulp.task('md5:css',['css'] ,function (done) {
         .on('end', done);
 });
 
- gulp.task('clean', function () {
+ gulp.task('clean', function (done) {
     gulp.src(['dest'])
         .pipe(clean())
+
+});
+ gulp.task('open', function (done) {
+    gulp.src('')
+        .pipe(gulpOpen({
+            app: browser,
+            uri: 'http://localhost:3000/'
+        }))
+        .on('end', done);
+});
+ gulp.task('connect', function () {
+    console.log('connect------------');
+    connect.server({
+        root: host.path,
+        port: host.port,
+        livereload: true
+    });
 });
 // 默认任务
 gulp.task('default',['clean'], function(){
-  gulp.run('img', 'css','lint', 'js', 'html', 'md5:css', 'md5:js');
+  gulp.run('connect','img', 'css','lint', 'js', 'html', 'md5:css', 'md5:js','open');
  
   // 监听html文件变化
   gulp.watch('src/*.html', function(){
@@ -106,4 +138,22 @@ gulp.task('default',['clean'], function(){
  
   // Watch image files
   gulp.watch('src/images/*', ['img']);
+});
+// 默认任务
+gulp.task('dev',function(){
+    gulp.run('connect','img', 'css','lint', 'js', 'html','open');
+
+    // 监听html文件变化
+    gulp.watch('src/*.html', function(){
+        gulp.run('html');
+    });
+
+    // Watch .css files
+    gulp.watch('src/css/*.css', ['css']);
+
+    // Watch .js files
+    gulp.watch('src/js/*.js', ['lint', 'js']);
+
+    // Watch image files
+    gulp.watch('src/images/*', ['img']);
 });
